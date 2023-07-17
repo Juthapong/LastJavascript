00603e92180a9c51669a6f6ad75178360dceee7d
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-07-18 00:51:59 +0700

    crate

---
+++ Lecture-03-ArrayLoopObject/Excercise/Ex-02.js
@@ -0,0 +1,33 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+for (let name of furniture) {
+    console.log(name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let item of furniture) {
+//     for (let letter of item) {
+//       console.log(letter);
+//     }
+//   }
+
+// const furniture = ['Table', 'Chairs', 'Couch']
+// furniture.forEach(e => console.log(e.split('')))
