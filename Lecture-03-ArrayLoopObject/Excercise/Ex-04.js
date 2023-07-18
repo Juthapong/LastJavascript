1b31037cc761e69cadf58fc80dc7008baf6fae2f
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-07-18 19:33:32 +0700

    update

---
+++ Lecture-03-ArrayLoopObject/Excercise/Ex-04.js
@@ -0,0 +1,6 @@
+function numberSum(arr) {
+    
+}
+console.log(numberSum([1, 2, "13", "4", "645"]));
+console.log(numberSum([true, false, "123", "75"]));
+console.log(numberSum([1, 2, 3, 4, 5, true]));
