2c0a649572d78fab9f0dd687aade46a2846e268d
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-07-24 17:23:34 +0700

    update

--- Lecture-03-ArrayLoopObject/Excercise/Ex-05.js
+++ Lecture-03-ArrayLoopObject/Excercise/Ex-05.js
@@ -11,9 +11,7 @@ function countPosSumNeg(arr) {
   }
   return arr.length === 0 ? [] : [positiveNum, negativeNum];
 }
-console.log(
-  countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
-);
+console.log(countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
 console.log(countPosSumNeg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34]));
 console.log(countPosSumNeg([91, -4, 80, -73, -28]));
 console.log(countPosSumNeg([]));
