75e4a54880c419a15719381209674757e463b7bb
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-08-22 09:36:39 +0700

    up

--- Lecture-B1-MidtermRecap/07-sumTwoSmallestNums.js
+++ Lecture-B1-MidtermRecap/07-sumTwoSmallestNums.js
@@ -17,7 +17,7 @@ function sumTwoSmallestNums(numbers) {
 }
 
 console.log(sumTwoSmallestNums([19, 5, 42, 2, 771])); // Output: 7
-console.log(sumTwoSmallestNums([10, 343445353, 3453445, 3453545353453])); // Output: 3453445
+console.log(sumTwoSmallestNums([10, 343445353, 3453445, 3453545353453])); // Output: 3453455
 console.log(sumTwoSmallestNums([2, 9, 6, -1])); // Output: 8
-console.log(sumTwoSmallestNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587])); // Output: 513
+console.log(sumTwoSmallestNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587])); // Output: 563
 console.log(sumTwoSmallestNums([3683, 2902, 3951, -475, 1617, -2385])); // Output: 4519
---
+++ Lecture-B1-MidtermRecap/1.js
@@ -0,0 +1,13 @@
+function totalVolume(...boxes) {
+    let total=0;
+    for (const box of boxes){
+        const [length,width,height]=box;
+        const volume = length*width*height;
+        total += volume;
+    }
+    return total;
+}
+
+console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]));
+console.log(totalVolume([2, 2, 2], [2, 1, 1]));
+console.log(totalVolume([1, 1, 1])); 
